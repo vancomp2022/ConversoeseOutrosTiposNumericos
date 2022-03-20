3e0c3259770c281cb43e74e69f296b7f09b6d087
Vanessa Soares de Morais <vsmdantas@outlook.com>
2022-03-20 08:54:12 -0300

    Adicionar arquivos de projeto.

---
+++ ConversoeseOutrosTiposNumericos/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+namespace ConversoesEOutrosTiposNumericos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o Projeto 4");
+
+            double salario;
+            salario = 1200.50;
+
+            int salarioEmInteiro;
+            salarioEmInteiro = (int)salario;
+
+            Console.WriteLine(salarioEmInteiro);
+
+            int idade;
+            idade = 26;
+
+
+            Console.ReadLine();
+        }
+    }
+}
